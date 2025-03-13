51c0bc15437ae89189d1638fec359b2c744283b5
franGiannico <fjgiannico@gmail.com>
2025-03-13 17:39:58 -0300

    Fix redirección en Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    historyApiFallback: true, // Permite redirecciones en una SPA
+  },
+  build: {
+    outDir: "dist", // Asegura que el build se guarde en "dist"
+  },
+});
