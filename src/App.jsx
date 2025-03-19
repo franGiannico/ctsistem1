3344630eb151db61ef1089445fd5986fa2502bb8
franGiannico <fjgiannico@gmail.com>
2025-03-18 22:01:31 -0300

    fix importación de header en app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx"; // ✅ Debe ser una importación default
 import Home from "./pages/Home"; // Asegúrate de crear este componente
 import ApiventasPage from "./pages/Apiventas";
 import ApiingresosPage from "./pages/Apiingresos";
