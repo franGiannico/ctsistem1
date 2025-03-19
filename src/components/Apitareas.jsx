9d6c96c764ceb3f6523b40cb725c793607cfb57b
franGiannico <fjgiannico@gmail.com>
2025-03-18 23:56:17 -0300

    Revert "Fix: Ancho completo en Apitareas"
    This reverts commit 2bc448b9f692037b44e1c83444a560c3c2d8a55e.

--- src/components/Apitareas.jsx
+++ src/components/Apitareas.jsx
@@ -80,7 +80,7 @@ export default function Apitareas() {
     };
 
     return (
-        <Container fluid className="mt-4 px-4">
+        <Container className="mt-4">
             <h2 className="text-center">Gestión de Tareas</h2>
             <Row className="mb-3">
                 <Col md={6} className="mx-auto">
