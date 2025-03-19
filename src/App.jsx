a5c254655118ba959085b04b0309df8a96fe74c3
franGiannico <fjgiannico@gmail.com>
2025-03-18 21:53:58 -0300

    agregado de header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home"; // Asegúrate de crear este componente
 import ApiventasPage from "./pages/Apiventas";
 import ApiingresosPage from "./pages/Apiingresos";
@@ -9,6 +10,7 @@ import ApitareasPage from "./pages/Apitareas";
 function App() {
   return (
     <Router>
+      <Header /> {/* 🔹 Agregamos el Header aquí */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/ventas" element={<ApiventasPage />} />
