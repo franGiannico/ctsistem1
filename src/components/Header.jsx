c0b5c53cc21edda22013552920e6dc7bba02eed3
franGiannico <fjgiannico@gmail.com>
2025-03-18 22:14:46 -0300

    fix error header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+const Header = () => {
+    return (
+        <Navbar bg="primary" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">Sistema CT</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={Link} to="/ventas">Ventas</Nav.Link>
+                        <Nav.Link as={Link} to="/ingresos">Ingresos</Nav.Link>
+                        <Nav.Link as={Link} to="/tareas">Tareas</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default Header;
