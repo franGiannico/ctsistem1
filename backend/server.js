2f5f8e341f37eae20cbe3713b78c3ed80cad4e09
franGiannico <fjgiannico@gmail.com>
2025-04-01 14:46:01 -0300

    agregado de host=ctsistem1-e68664e8ae46.herokuapp.com a cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // 🔹 Habilitar CORS para permitir peticiones desde el frontend
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://ctsistem1.herokuapp.com', "https://ctsistem1.netlify.app"],
+  origin: ['http://localhost:5173', 'https://ctsistem1.herokuapp.com', "https://ctsistem1.netlify.app", "host=ctsistem1-e68664e8ae46.herokuapp.com"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
   allowedHeaders: ["Content-Type"],
 }));
