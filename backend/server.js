e935fcf7b19ff3198ff7f5a819e1353e1ffe4c1e
franGiannico <fjgiannico@gmail.com>
2025-07-09 12:08:15 -0300

    corrección server

--- backend/server.js
+++ backend/server.js
@@ -64,10 +64,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 // 🚀 Iniciar el servidor
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
