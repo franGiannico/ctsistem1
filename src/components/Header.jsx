900a02cf9394fe88b9c82149d1728bbed8ab491f
Francisco Giannico <fjgiannico@gmail.com>
2026-09-09 14:19:11 +0000

    Fix: cerrar menu mobile automaticamente al navegar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
 // file: src/components/Header.jsx
 
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 
 const Header = () => {
+    const [expanded, setExpanded] = useState(false);
+
     return (
-        <Navbar bg="primary" variant="dark" expand="lg">
+        <Navbar bg="primary" variant="dark" expand="lg" expanded={expanded} onToggle={setExpanded}>
             <Container>
-                <Navbar.Brand as={Link} to="/">Sistema CT</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>Sistema CT</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link as={Link} to="/ventas">Ventas</Nav.Link>
-                        <Nav.Link as={Link} to="/ingresos">Ingresos</Nav.Link>
-                        <Nav.Link as={Link} to="/tareas">Tareas</Nav.Link>
+                        <Nav.Link as={Link} to="/ventas" onClick={() => setExpanded(false)}>Ventas</Nav.Link>
+                        <Nav.Link as={Link} to="/ingresos" onClick={() => setExpanded(false)}>Ingresos</Nav.Link>
+                        <Nav.Link as={Link} to="/tareas" onClick={() => setExpanded(false)}>Tareas</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
